09b86df003f91a42f04b45475ad5628e8e5dc617
saddam <bseddam@gmail.com>
2019-05-08 10:42:16 +0400

    dil

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -20,14 +20,27 @@ public partial class MasterPage : System.Web.UI.MasterPage
 
 
 
+
+        //if (lang == "en")
+        //{
+        //    ltrLink.Text = string.Format("<input onclick='location.href=&apos;{0}&apos;' type='button' class='btn btn-az' value='AZ' /> " +
+        //        "<input onclick='location.href=&apos;{1}&apos;' type='button' class='btn btn-az' value='EN' />", getURL, getURL);
+        //}
+        //else
+
+        //{
+        //    ltrLink.Text = string.Format("<input onclick='location.href=&apos;{0}&apos;' type='button' class='btn btn-az' value='AZ' /> " +
+        //        "<input onclick='location.href=&apos;{1}&apos;' type='button' class='btn btn-az' value='EN' />", getURL, getURL);
+        //}
+
         if (lang == "en")
         {
-            ltrLink.Text = string.Format("<a  class='btn btn-default' href='{0}'>AZ</a>" +
+            ltrLink.Text = string.Format("<a  class='btn btn-default' href='{0}'>AZ</a> " +
                 "<a class='btn btn-default active' href='{1}' >EN</a>", getURL, getURL);
         }
         else
         {
-            ltrLink.Text = string.Format("<a class='btn btn-default active' href='{0}'>AZ</a>" +
+            ltrLink.Text = string.Format("<a class='btn btn-default active' href='{0}'>AZ</a> " +
                 "<a class='btn btn-default' href='{1}'>EN</a>", getURL, getURL);
         }
         DataTable dtHeader = db.GetMenuClient(Utils.MenuType.Header, lang);
