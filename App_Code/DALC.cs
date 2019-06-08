55eccce65efce285d813172fa12af4708fcfffd9
saddam <bseddam@gmail.com>
2019-06-08 21:18:07 +0400

    indi hazirdir

--- App_Code/DALC.cs
+++ App_Code/DALC.cs
@@ -3166,7 +3166,40 @@ on i.goal_id=i3.goal_id", SqlConn);
         }
     }
 
+    public DataTable GetIndicatorsReportingStatus2()
+    {
+        try
+        {
 
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(@"
+select i1.name_az prioritetdir_az,i2.name_az prioritetdeyil_az,
+i1.name_en prioritetdir_en,i2.name_en prioritetdeyil_en,i.*,i1.prioritetdir,i2.prioritetdeyil,
+cast(i1.prioritetdir*100/i.cemisay as decimal(6,1)) faizprioritetdir,
+cast(i2.prioritetdeyil*100/i.cemisay as decimal(6,1)) faizprioritetdeyil
+from (
+SELECT i.goal_id,g.name_short_az,g.name_short_en,count(*) cemisay FROM `indicators` i 
+inner join goals g on i.goal_id=g.id 
+where i.type_id=1 and i.parent_id=0 and i.isActive=1 
+group by i.goal_id,g.name_short_az,g.name_short_en) as i 
+inner join (SELECT i.goal_id,s.name_az,s.name_en,count(*) prioritetdir FROM `indicators` as i 
+inner join indicator_uygunluq1 as s on i.uygunluq_id=s.id where i.type_id=1 and i.parent_id=0 and 
+i.uygunluq_id=1 and i.isActive=1 group by i.goal_id,s.name_az,s.name_en) as i1 on i.goal_id=i1.goal_id
+inner join (SELECT i.goal_id,s.name_az,s.name_en,count(*) prioritetdeyil FROM `indicators` as i 
+inner join indicator_uygunluq1 as s on i.uygunluq_id=s.id where i.type_id=1 and i.parent_id=0 and 
+i.uygunluq_id=2 and i.isActive=1 group by i.goal_id,s.name_az,s.name_en) as i2 on i.goal_id=i2.goal_id", SqlConn);
+
+
+
+            da.Fill(dt);
+            return dt;
+        }
+        catch (Exception ex)
+        {
+            LogInsert(Utils.Tables.goals, Utils.LogType.select, String.Format("GetIndicatorsReportingStatus2()"), ex.Message, "", true);
+            return null;
+        }
+    }
 
 
     public DataTable GetTargetsReportingStatusPriotet()
@@ -3244,6 +3277,9 @@ i.uygunluq_id=2 and i.isActive=1 group by i.goal_id,s.name_az,s.name_en) as i2 o
             return null;
         }
     }
+
+
+    
 
 
     public DataTable GetTargetsReportingStatusSum()
